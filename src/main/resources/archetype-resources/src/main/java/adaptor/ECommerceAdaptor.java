6de1108062869d964fdfb4a165a68e91c823be27
Venky <venkatramanm@gmail.com>
2023-03-15 15:26:49 +0530

    Fixes

--- src/main/resources/archetype-resources/src/main/java/adaptor/ECommerceAdaptor.java
+++ src/main/resources/archetype-resources/src/main/java/adaptor/ECommerceAdaptor.java
@@ -1,163 +1,64 @@
 package ${package}.adaptor;
 
-import com.venky.core.collections.IgnoreCaseMap;
-import com.venky.core.string.StringUtil;
-import com.venky.core.util.ObjectUtil;
-import com.venky.swf.db.Database;
 import com.venky.swf.plugins.beckn.messaging.Subscriber;
-import com.venky.swf.sql.Expression;
-import com.venky.swf.sql.Operator;
-import com.venky.swf.sql.Select;
-import in.succinct.beckn.BecknStrings;
-import in.succinct.beckn.Catalog;
-import in.succinct.beckn.Categories;
-import in.succinct.beckn.Category;
-import in.succinct.beckn.Descriptor;
-import in.succinct.beckn.Fulfillment;
-import in.succinct.beckn.Fulfillment.FulfillmentType;
-import in.succinct.beckn.Fulfillments;
-import in.succinct.beckn.Intent;
-import in.succinct.beckn.Item;
 import in.succinct.beckn.Items;
-import in.succinct.beckn.Location;
 import in.succinct.beckn.Locations;
-import in.succinct.beckn.Message;
 import in.succinct.beckn.Order;
-import in.succinct.beckn.Payment;
-import in.succinct.beckn.Payments;
-import in.succinct.beckn.Provider;
-import in.succinct.beckn.Providers;
-import in.succinct.beckn.Quote;
 import in.succinct.beckn.Request;
 import in.succinct.bpp.core.adaptor.CommerceAdaptor;
-import in.succinct.bpp.core.registry.BecknRegistry;
-import in.succinct.bpp.search.adaptor.SearchAdaptor;
-import ${package}.db.model.BecknOrderMeta;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import in.succinct.bpp.core.db.model.BecknOrderMeta;
 
-import java.util.List;
 import java.util.Map;
 
 public class ECommerceAdaptor extends CommerceAdaptor {
-    final SearchAdaptor searchAdaptor;
-    final Map<String,String> configuration ;
-    public ECommerceAdaptor(Map<String,String> configuration, Subscriber subscriber, BecknRegistry registry){
-        super(subscriber,registry);
-        this.searchAdaptor = new SearchAdaptor(this);
-        this.configuration = configuration;
-    }
+     public ECommerceAdaptor(Map<String,String> configuration, Subscriber subscriber){
+        super(configuration,subscriber);
+         getProviderConfig().getSupportContact().setEmail(getSupportEmail());
+         getProviderConfig().setLocation(getProviderLocations().get(0));
+     }
 
-    public Map<String, String> getConfiguration() {
-        return configuration;
-    }
-
-    @Override
-    public void search(Request request,Request reply){
-        this.searchAdaptor.search(request,reply);
-    }
-
-    /* Don't remove Used from AppInstaller */
-    /* Search is fulfilled from the plugin */
-    public void _search(Request request, Request reply) {
-        /* Code here logic to seach your ecommerce application like woocommerce,shopify, ...*/
-        /* Return reply compatible with on_search json */
+    private String getSupportEmail() {
+         return null; //Get from Application.
     }
 
     @Override
-    public void select(Request request, Request response) {
-        /* Take select json and fill response with on_select message */
-
+    public Locations getProviderLocations() {
+        return null;
     }
 
     @Override
-    public void init(Request request, Request reply) {
-        Order order = request.getMessage().getOrder();
-        if (order == null){
-            throw new RuntimeException("No Order passed");
-        }
-        /* Take init message and fill response with on_init message */
-
+    public Items getItems() {
+        return null;
     }
 
     @Override
-    public void confirm(Request request, Request reply) {
-        Order order = request.getMessage().getOrder();
-        if (order == null){
-            throw new RuntimeException("No Order passed");
-        }
-        /* Take confirm message and fill response with on_confirm message */
+    public boolean isTaxIncludedInPrice() {
+        return false;
     }
 
     @Override
-    public void track(Request request, Request reply) {
-        /* Take track message and fill response with on_track message */
-
+    public Order initializeDraftOrder(Request request) {
+        return null;
     }
 
     @Override
-    public void cancel(Request request, Request reply) {
-        Order order = request.getMessage().getOrder();
-        if (order == null){
-            throw new RuntimeException("No Order passed");
-        }
-        /* Take cancel message and fill response with on_cancel message */
+    public Order confirmDraftOrder(Order draftOrder, BecknOrderMeta meta) {
+        return null;
     }
 
     @Override
-    public void update(Request request, Request reply) {
-        throw new RuntimeException("Orders cannot be updated. Please cancel and rebook your orders!");
+    public Order getStatus(Order order) {
+        return null;
     }
 
     @Override
-    public void status(Request request, Request reply) {
-        Order order = request.getMessage().getOrder();
-        if (order == null){
-            order = new Order();
-            order.setId(request.getMessage().get("order_id"));
-            request.getMessage().setOrder(order);
-        }
-        if (order == null){
-            throw new RuntimeException("No Order passed");
-        }
-        /* Take status message and fill response with on_status message */
-
+    public Order cancel(Order order) {
+        return null;
     }
 
     @Override
-    public void rating(Request request, Request reply) {
-
-    }
-
-
-    @Override
-    public void support(Request request, Request reply) {
-        
-    }
-
-    @Override
-    public void get_cancellation_reasons(Request request, Request reply) {
-
-    }
-
-    @Override
-    public void get_return_reasons(Request request, Request reply) {
-
-    }
-
-    @Override
-    public void get_rating_categories(Request request, Request reply) {
-
-    }
-
-    @Override
-    public void get_feedback_categories(Request request, Request reply) {
-
-    }
-
-    @Override
-    public void get_feedback_form(Request request, Request reply) {
-
+    public String getTrackingUrl(Order order) {
+        return null;
     }
 
 
--- src/main/resources/archetype-resources/src/main/java/extensions/Webhook.java
+++ src/main/resources/archetype-resources/src/main/java/extensions/Webhook.java
@@ -6,23 +6,20 @@ import com.venky.core.util.ObjectUtil;
 import com.venky.extension.Extension;
 import com.venky.extension.Registry;
 import com.venky.swf.path.Path;
-import com.venky.swf.routing.Config;
 import in.succinct.beckn.Context;
-import in.succinct.beckn.Order;
 import in.succinct.beckn.Message;
+import in.succinct.beckn.Order;
 import in.succinct.beckn.Request;
 import in.succinct.bpp.core.adaptor.CommerceAdaptor;
+import in.succinct.bpp.core.adaptor.NetworkAdaptor;
 import ${package}.adaptor.ECommerceAdaptor;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.JSONValue;
+
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.UUID;
-import java.util.logging.Level;
 
 public class Webhook implements Extension {
     static {
@@ -31,26 +28,36 @@ public class Webhook implements Extension {
     @Override
     public void invoke(Object... objects) {
         CommerceAdaptor adaptor = (CommerceAdaptor) objects[0];
-        Path path = (Path) objects[1];
+        NetworkAdaptor networkAdaptor = (NetworkAdaptor)objects[1];
+        Path path = (Path) objects[2];
         if (!(adaptor instanceof ECommerceAdaptor)) {
             return;
         }
         ECommerceAdaptor eCommerceAdaptor = (ECommerceAdaptor) adaptor;
         try {
-            hook(eCommerceAdaptor, path);
+            hook(eCommerceAdaptor, networkAdaptor,path);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
     }
-    public void hook(ECommerceAdaptor eCommerceAdaptor,Path path) throws Exception{
+    public void hook(ECommerceAdaptor eCommerceAdaptor, NetworkAdaptor networkAdaptor,Path path) throws Exception{
         String payload = StringUtil.read(path.getInputStream());
         //Validate auth headers from path.getHeader
-        if (ObjectUtil.equals(path.getHeader("X-WC-Webhook-Topic"),"order.updated")){
-            JSONObject eOrder = (JSONObject) JSONValue.parse(payload);
-            Order becknOrder = new Order(); //Fill all attributes here. 
+        String sign = path.getHeader("X-Hmac-SHA256");
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(
+                    eCommerceAdaptor.getConfiguration().get("${package}.hmac.key").getBytes(StandardCharsets.UTF_8),
+                    "HmacSHA256"));
+        byte[] hmacbytes = mac.doFinal(payload.getBytes(StandardCharsets.UTF_8));
+        if (!ObjectUtil.equals(Crypt.getInstance().toBase64(hmacbytes),sign)){
+            throw new RuntimeException("Webhook - Signature failed!!");
+        }
            
+        if (path.action().equals("order_hook")){
+            String event = path.parameter();
 
 
+            Order becknOrder = new Order();// fillRest.
             final Request request = new Request();
             request.setMessage(new Message());
             request.setContext(new Context());
@@ -60,10 +67,11 @@ public class Webhook implements Extension {
             context.setBppUri(eCommerceAdaptor.getSubscriber().getSubscriberUrl());
             context.setTimestamp(new Date());
             context.setAction("on_status");
+            context.setDomain(eCommerceAdaptor.getSubscriber().getDomain());
             //Fill any other attributes needed.
             //Send unsolicited on_status.
             context.setMessageId(UUID.randomUUID().toString());
-            eCommerceAdaptor.callback(request);
+            networkAdaptor.getApiAdaptor().callback(eCommerceAdaptor,request);
 
         }
     }
